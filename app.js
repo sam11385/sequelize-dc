d40bc42f5b4110a8aed211153bb6aa00d348ec6d
sam11385 <samluedke@gmail.com>
2019-01-03 17:48:19 -0500

    Build examples

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
 
+const models = require('./models');
+
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.get('/', function(req, res) {
-  res.render('home', {
-    pageTitle: 'Home',
-    pageID: 'Home'
-  });
+  // FIND ALL EXAMPLE
+  // models.user.findAll().then(results => {
+  //   results.forEach(function(index) {
+  //     console.log(index.id, index.firstName);
+  //   });
+  // });
+});
+
+// create a user example
+// models.user
+//   .create({ firstName: 'Sam', lastName: 'Luedke' })
+//   .then(function(user) {
+//     console.log(user);
+//   });
+
+// Query full table
+// models.user.findAll().then(results => {
+//   const r = results;
+//   console.log(r);
+// });
+
+// Query single row from table
+models.user.findAll().then(results => {
+  const r = results[0];
+  console.log(r.id, r.firstName);
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+  const user = sequelize.define(
+    'user',
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
   user.associate = function(models) {
     // associations can be defined here
   };
